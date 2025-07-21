7d5b3966e8097eecb1269c1c32adbcdbc9f71ac9
Joshua Vilog <joshuanisperosvilog@gmail.com>
2025-07-21 11:39:35 +0800

    2025-07-21 first commit

---
+++ config/path.php
@@ -0,0 +1,10 @@
+<?php
+
+$rootFolder = "1_PMMS";
+// $pluginFolder = $rootFolder."/assets";
+$pluginFolder = "2_VILOG_ASSETS";
+$systemTitle = "PMMS";
+$generateRandomNumber = rand(1000,9999);
+
+
+?>
